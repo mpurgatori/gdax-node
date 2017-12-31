35978e06b8ba7edc9c1f1850c2a7dc442d0c4312
Mike Purgatori <mpurgatori@gmail.com>
2017-12-31 12:03:03 -0600

    writing to .txt

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-module.exports = exports = {
-  PublicClient: require('./lib/clients/public.js'),
-  WebsocketClient: require('./lib/clients/websocket.js'),
-  AuthenticatedClient: require('./lib/clients/authenticated.js'),
-  Orderbook: require('./lib/orderbook.js'),
-  OrderbookSync: require('./lib/orderbook_sync.js'),
-};
+const WebsocketClient = require('./lib/clients/websocket.js');
+const fs = require('fs');
+
+const fileName = new Date();
+
+const websocket = new WebsocketClient(['BTC-USD', 'ETH-USD']);
+websocket.on('message', data => {
+  const eachLine = `${data.type},${data.order_id},${data.order_type},${
+    data.size
+  },${data.remaining_size},${data.price},${data.side},${data.reason},${
+    data.client_oid
+  },${data.product_id},${data.sequence},${data.time},\n`;
+  fs.appendFile(
+    `./gdax_data_stream/${fileName}.txt`,
+    eachLine,
+    'ascii',
+    err => {
+      if (err) {
+        throw err;
+      }
+    }
+  );
+});
