eff56b90deb54bc507054a8e728e92348645e14e
Mike Purgatori <mpurgatori@gmail.com>
2017-12-31 12:04:18 -0600

    removed prettier script

--- index.js
+++ index.js
@@ -4,20 +4,14 @@ const fs = require('fs');
 const fileName = new Date();
 
 const websocket = new WebsocketClient(['BTC-USD', 'ETH-USD']);
-websocket.on('message', data => {
-  const eachLine = `${data.type},${data.order_id},${data.order_type},${
-    data.size
-  },${data.remaining_size},${data.price},${data.side},${data.reason},${
-    data.client_oid
-  },${data.product_id},${data.sequence},${data.time},\n`;
-  fs.appendFile(
-    `./gdax_data_stream/${fileName}.txt`,
-    eachLine,
-    'ascii',
-    err => {
-      if (err) {
-        throw err;
-      }
-    }
-  );
+websocket.on('message', data => { 
+const eachLine = `${data.type},${data.order_id},${data.order_type},${data.size},${data.remaining_size},${data.price},${data.side},${data.reason},${data.client_oid},${data.product_id},${data.sequence},${data.time},\n`
+ fs.appendFile(`./gdax_data_stream/${fileName}.txt`, eachLine,'ascii', (err) => {  
+    if (err) {throw err;}
+  });
 });
+
+
+
+
+
