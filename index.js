b9df543c88440798384d1e9a3635cff6943cfde8
Mike Purgatori <mpurgatori@gmail.com>
2018-01-02 21:49:17 -0600

    date modify

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const WebsocketClient = require('./lib/clients/websocket.js');
 const fs = require('fs');
 
-const fileName = new Date();
+let fileName = new Date();
+
+setInterval(function(){ // Set interval for checking
+  var date = new Date(); // Create a Date object to find out what time it is
+  if(date.getHours() === 6 && date.getMinutes() === 0){ // Check the time
+    fileName = new Date();
+  }
+}, 60000);
 
 const websocket = new WebsocketClient(['BTC-USD', 'ETH-USD']);
 websocket.on('message', data => { 
@@ -10,7 +17,6 @@ const eachLine = `${data.type},${data.order_id},${data.order_type},${data.size},
     if (err) {throw err;}
   });
 });
-
 
 
 
